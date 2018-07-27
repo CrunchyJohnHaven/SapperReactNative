df9a8ee25814214cacf8d78ab398149f3dd9b704
John Bradley <johnhavenbradley@sappercoding.com>
2018-07-27 09:57:48 -0700

    update

--- SapperApp/App.js
+++ SapperApp/App.js
@@ -1,15 +1,82 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Button} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { createStackNavigator } from 'react-navigation';
+import NavBar, { NavButton, NavButtonText, NavTitle } from 'react-native-nav';
 
-export default class App extends React.Component {
+class NavBarIOSLight extends React.Component {
+  render() {
+    return (
+      <NavBar>
+        <NavButton onPress={() => alert('hi')}>
+          <NavButtonText>
+            {"Button"}
+          </NavButtonText>
+        </NavButton>
+        <NavTitle>
+          {"App"}
+        </NavTitle>
+        <NavButton onPress={() => alert('hi')}>
+          <NavButtonText>
+            {"Button"}
+          </NavButtonText>
+        </NavButton>
+      </NavBar>
+    )
+  }
+}
+
+class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={[styles.title]}>Quotesla</Text>
+        <Text>Build Your Vision</Text>
+        <Button style={[styles.containerStyle]}
+          title='HOME'
+          onPress={() => this.props.navigation.navigate('Details')}
+      />
       </View>
     );
+  }
+}
+class DashboardBody extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>DASHBOARD</Text>
+      </View>
+    )
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <NavBar>
+          <Text>HELLO</Text>
+        </NavBar>
+        <DashboardBody/>
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
   }
 }
 
@@ -19,5 +86,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+  title: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: '#030d41',
+    marginBottom: 10
+  },
+  containerStyle: {
+    marginTop: 20
+  }
 });
